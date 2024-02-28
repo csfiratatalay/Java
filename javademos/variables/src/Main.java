497245c131fdd3c3071a04b69611dc840580d282
csfiratatalay <csfiratatalay@gmail.com>
2024-02-28 22:56:47 +0300

    New Journey

---
+++ javademos/variables/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int myFirstNumber = 13;
+        String myName = "Firat Atalay";
+
+        System.out.println("My name is " + myName + "."  );
+        System.out.println("I am " + myFirstNumber + " years old.");
+    }
+}
