058b04391449a73606f823ad582011e3af1c8a38
csfiratatalay <csfiratatalay@gmail.com>
2024-02-29 00:16:11 +0300

    New Journey

---
+++ javademos/fakeHelloWorld/src/main/java/com/mycompany/fakehelloworld/FakeHelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fakehelloworld;
+
+/**
+ *
+ * @author FIRAT
+ */
+public class FakeHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
