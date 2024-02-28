5ede13018c81cfa3aa515157502c13407d34a2cf
csfiratatalay <csfiratatalay@gmail.com>
2024-02-28 23:09:19 +0300

    New Journey

---
+++ javademos/dataTypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int semester = 2;
+        String myName = "Firat";
+        boolean isLike = false;
+        double hourToFinishCourse = 13.13131313131313;
+
+        System.out.println("My name is " + myName + ". This is " + semester + ". semester." );
+
+        if(!isLike){
+            System.out.println(myName + " likes to watch course on the BTK Akademi.");
+            System.out.println("To finish this course, I have to watch " + hourToFinishCourse +" hours.");
+        }
+    }
+}
